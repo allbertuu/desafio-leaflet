52a25b8f4561c523b771d8ec2e0cedc1fe5b2c80
Alberto <albertovinicius3@gmail.com>
2022-05-23 09:35:00 -0300

    feat(h1): add font-bold

--- src/routes/Form.tsx
+++ src/routes/Form.tsx
@@ -59,7 +59,7 @@ function Form() {
 
       <LocationMarkerIcon className="text-white h-16 mb-4" />
 
-      <h1 className="text-3xl uppercase text-center">Bem-vindo!</h1>
+      <h1 className="text-3xl uppercase text-center font-bold">Bem-vindo!</h1>
 
       <h3 className="text-xl text-center mt-4">Cadastre um local</h3>
       <div className="my-5 mx-10">
